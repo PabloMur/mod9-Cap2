722e9b0307d1e68391bfaaf8655c6c8c8db57b64
PabloMur <polillomurillo@gmail.com>
2023-03-10 00:27:15 -0300

    making the firsts endpoints

---
+++ pages/api/auth/token.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { methods } from "micro-method-router";
+
+export default methods({
+  async post(req: NextApiRequest, res: NextApiResponse) {
+    res
+      .status(200)
+      .send(
+        "Recibe un email y encuentra/crea un user con ese email y le envía un código vía email."
+      );
+  },
+});
