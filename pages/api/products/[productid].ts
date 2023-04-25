a59a3f654a09f3ec2d6c64fd55d940eba5bd49a2
PabloMur <polillomurillo@gmail.com>
2023-04-25 16:59:01 -0300

    qwe

--- pages/api/products/[productid].ts
+++ pages/api/products/[productid].ts
@@ -3,6 +3,6 @@ import { methods } from "micro-method-router";
 
 export default methods({
   async get(req: NextApiRequest, res: NextApiResponse) {
-    return res.status(200).send("id del producto");
+    res.status(200).send("id del producto");
   },
 });
