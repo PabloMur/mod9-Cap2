40776f1adc222c0085a7f72b88b3ddad6044bf17
PabloMur <polillomurillo@gmail.com>
2023-04-25 17:31:40 -0300

    asd

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -3,6 +3,6 @@ import { method } from "micro-method-router";
 
 export default method({
   async get(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json("prod");
+    res.status(200).send("prod");
   },
 });
