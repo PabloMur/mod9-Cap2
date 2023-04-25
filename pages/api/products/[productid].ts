be024c9cdbd6068ab8c710ff7d874e6918edf5a0
PabloMur <polillomurillo@gmail.com>
2023-04-25 17:48:23 -0300

    testeando

--- pages/api/products/[productid].ts
+++ pages/api/products/[productid].ts
@@ -3,6 +3,6 @@ import methods from "micro-method-router";
 
 export default methods({
   async get(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).send("prod" + req.query);
+    res.status(200).send("prod" + req.query.params);
   },
 });
